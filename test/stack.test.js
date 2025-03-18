efd4705cd8a76099e3687b49702f3bd14f60d35a
daniel_eln <omoniyiopemipo6@gmail.com>
2025-03-18 10:26:31 +0100

    test(stack): wrote test cases from pushing and popping items from the stack

--- test/stack.test.js
+++ test/stack.test.js
@@ -3,6 +3,22 @@ class Stack {
         this.top = -1
         this.items = {}
     }
+
+    get peek() {
+        return this.items[this.top]
+    }
+
+    push(value) {
+        this.top += 1
+        this.items[this.top] = value
+    }
+
+    pop() {
+        const value = this.items[this.top]
+        delete this.items[this.top]
+        this.top = this.top - 1
+        return value
+    }
 } 
 
 describe("My stack", () => {
@@ -17,7 +33,17 @@ describe("My stack", () => {
         expect(stack.items).toEqual({});
     })
 
-    it.todo("Can push items to the top")
+    // it.todo("Can push items to the top")
+    it("Can push items to the top", () => {
+        stack.push("🍉");
+        expect(stack.top).toBe(0);
+        expect(stack.peek).toBe("🍉");
+    })
 
-    it.todo("Can pop items from the top")
+    // it.todo("Can pop items from the top")
+    it("Can pop items from the top", () => {
+        stack.push("🍉");
+        expect(stack.pop()).toBe("🍉");
+        expect(stack.top).toBe(-1);
+    })
 })
